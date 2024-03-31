17783860276514f1fbe00e1ecffa7b00c830bffa
mutuajames <njorog3.james@gmail.com>
2024-03-31 21:31:37 +0300

    Reformat and support static files in development

--- server/server/settings.py
+++ server/server/settings.py
@@ -37,7 +37,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "server.server.urls"
+ROOT_URLCONF = "server.urls"
 
 TEMPLATES = [
     {
@@ -106,7 +106,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
